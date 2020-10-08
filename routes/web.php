790a8fa3e91d883a6981a86078302ae910f127fc
Joilson Miranda S Lopes <joilsonmirandasl@hotmail.com>
2020-10-08 12:03:05 -0300

    Aula sobre rotas any e match

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,23 @@
 <?php
 
+/* A rota match, você pode setar qual ou quais os verbos http a serem usados dentro de um array */
+Route::match(['get', 'post'], '/match', function() {
+    return 'Match';
+});
+
+/* A rota any, é aceito qualquer tipo de método, seja, get, post, put, patch, delete ... */
+Route::any('/any', function () {
+    return 'Any';
+});
+
+Route::get('/empresa', function() {
+    return view('site.empresa');
+});
+
+Route::get('/products', function() {
+    return view('products');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
