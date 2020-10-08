2c57391e15526bd78e97cedb8f105a73a39d6765
Joilson Miranda S Lopes <joilsonmirandasl@hotmail.com>
2020-10-08 20:29:32 -0300

    Como passar parametros no laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,19 @@
 <?php
 
+/* Passando parâmetro opcional */
+Route::get('/produtos/{idProduct?}/servico', function($idProduct = '') {
+    return "Produto(s): {$idProduct}";
+});
+
+/* Passando rotas com parâmetro */
+Route::get('/categoria/{flag}/posts', function($flag) {
+    return "Posts da categoria: {$flag}";
+});
+
+Route::get('/categorias/{flag}', function($flag) {
+    return "Produtos da categoria: {$flag}";
+});
+
 /* A rota match, você pode setar qual ou quais os verbos http a serem usados dentro de um array */
 Route::match(['get', 'post'], '/match', function() {
     return 'Match';
@@ -14,7 +28,7 @@ Route::get('/empresa', function() {
     return view('site.empresa');
 });
 
-Route::get('/products', function() {
+Route::get('/produto', function() {
     return view('products');
 });
 
